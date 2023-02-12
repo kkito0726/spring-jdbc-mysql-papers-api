d97c61d54889e802674542ac58616ed97f895fbe
kkito0726 <k.kito0726@gmail.com>
2023-02-12 10:53:37 +0000

    spring bootプロジェクトの雛形を作成しHealthのエンドポイントを作った

---
+++ papers-api/src/main/java/com/ken/papersapi/PapersApiApplication.java
@@ -0,0 +1,13 @@
+package com.ken.papersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapersApiApplication.class, args);
+	}
+
+}
---
+++ papers-api/src/test/java/com/ken/papersapi/PapersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ken.papersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
