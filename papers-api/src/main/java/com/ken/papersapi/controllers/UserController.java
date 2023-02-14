5aee703337c5c3e6be22c6625a427505d038e530
kkito0726 <k.kito0726@gmail.com>
2023-02-14 07:31:20 +0000

    アカウント登録のAPIを追加

---
+++ papers-api/src/main/java/com/ken/papersapi/controllers/UserController.java
@@ -0,0 +1,59 @@
+package com.ken.papersapi.controllers;
+
+import com.ken.papersapi.dtos.UpdateUserDto;
+import com.ken.papersapi.models.User;
+import com.ken.papersapi.services.UserService;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = UserController.BASE_URL)
+@RequiredArgsConstructor
+public class UserController {
+
+  private static final String BASE_URL = "/api/v1/users";
+  private final UserService userService;
+
+  @GetMapping("")
+  public ResponseEntity<List<User>> getUsers() {
+    List<User> users = userService.findAll();
+    return ResponseEntity.ok(users);
+  }
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<User> getUser(@PathVariable UUID userId) {
+    User user = userService.findByUserid(userId);
+    return ResponseEntity.ok(user);
+  }
+
+  @PostMapping("")
+  public ResponseEntity<User> createUser(@RequestBody User user) {
+    User newUser = userService.save(user);
+    return ResponseEntity.ok(newUser);
+  }
+
+  @PatchMapping("/{userId}")
+  public ResponseEntity<Object> updateUser(
+    @RequestBody UpdateUserDto dto,
+    @PathVariable UUID userId
+  ) {
+    userService.updateByUserId(dto, userId);
+    return ResponseEntity.noContent().build();
+  }
+
+  @DeleteMapping("/{userId}")
+  public ResponseEntity<Object> deleteUser(@PathVariable UUID userId) {
+    userService.delete(userId);
+    return ResponseEntity.noContent().build();
+  }
+}
--- papers-api/src/main/java/com/ken/papersapi/services/UserService.java
+++ papers-api/src/main/java/com/ken/papersapi/services/UserService.java
@@ -1,10 +1,16 @@
 package com.ken.papersapi.services;
 
+import com.ken.papersapi.dtos.UpdateUserDto;
 import com.ken.papersapi.models.User;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +20,59 @@ public class UserService {
   private JdbcTemplate jdbcTemplate;
 
   public List<User> findAll() {
-    final String query = "SELECT * from user";
+    final String query = "SELECT * from users WHERE deleted_at is NULL";
     List<User> users = jdbcTemplate.query(
       query,
       new BeanPropertyRowMapper<>(User.class)
     );
 
     return users;
+  }
+
+  public User findByUserid(UUID userId) {
+    final String query = "SELECT * from users WHERE user_id=?";
+    List<User> users = jdbcTemplate.query(
+      query,
+      new BeanPropertyRowMapper<>(User.class),
+      userId.toString()
+    );
+
+    return users.get(0);
+  }
+
+  public void updateByUserId(UpdateUserDto dto, UUID userId) {
+    final String query =
+      "UPDATE users SET name=?, email=?, password=? WHERE user_id=?";
+
+    jdbcTemplate.update(
+      query,
+      dto.getName(),
+      dto.getEmail(),
+      dto.getPassword(),
+      userId.toString()
+    );
+  }
+
+  public void delete(UUID userId) {
+    final String query = "UPDATE users SET deleted_at=? WHERE user_id=?";
+    jdbcTemplate.update(query, LocalDateTime.now(), userId.toString());
+  }
+
+  public User save(User user) {
+    onCreateUser(user);
+    SqlParameterSource param = new BeanPropertySqlParameterSource(user);
+    SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate)
+      .withTableName("users")
+      .usingGeneratedKeyColumns("no");
+
+    Number key = insert.executeAndReturnKey(param);
+    user.setNo(key.hashCode());
+
+    return user;
+  }
+
+  public void onCreateUser(User user) {
+    user.setUserId(UUID.randomUUID());
+    user.setCreatedAt(LocalDateTime.now());
   }
 }
